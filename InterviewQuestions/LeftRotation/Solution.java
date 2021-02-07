4d2064188b357330c911782cf7d3e52170557af9
Trui-git <tengrui13@gmail.com>
2021-02-07 13:05:02 -0500

    LeftRotation

--- InterviewQuestions/LeftRotation/Solution.java
+++ InterviewQuestions/LeftRotation/Solution.java
@@ -1,18 +1,38 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.math.*;
 import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
+import java.util.stream.Collectors;
 
 public class Solution {
 
     // Complete the rotLeft function below.
     static int[] rotLeft(int[] a, int d) {
-        int [] results = new int[a.length];
+        
+        int [] result = new int[a.length];        
+        List<Integer> temp =  Arrays.stream(a).boxed().collect(Collectors.toList());
+        List<Integer> temp1 = new ArrayList<Integer>();
+        temp1.addAll(temp.subList(0,d));
 
-        return results;
+        List<Integer> temp2 = new ArrayList<Integer>();
+        temp2.addAll(temp.subList(d, a.length));
+
+        System.out.println(temp1);
+        System.out.println(temp2);
+
+        temp.clear();
+        temp.addAll(temp2);
+        temp.addAll(temp1);
+        System.out.println(temp);
+        
+        result = temp.stream().mapToInt(Integer::intValue).toArray();
+        System.out.println(result);
+
+        return result;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
