7e7f913143c864b95f9494bd5e777469d0f13f80
ruiteng2021 <tengrui13@gmail.com>
2021-11-19 11:07:58 -0500

    Add old stuffs

---
+++ Javascript/test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'acmTeam' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts STRING_ARRAY topic as parameter.
+ */
+
+function acmTeam(topic) {
+    // Write your code here
+    console.log("Hello world");
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    let topic = [];
+
+    for (let i = 0; i < n; i++) {
+        const topicItem = readLine();
+        topic.push(topicItem);
+    }
+
+    const result = acmTeam(topic);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
